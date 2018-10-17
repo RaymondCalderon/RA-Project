56fe72435c6162bd00745886c99623c805bbbb9c
AngelNegrete <32958423+AngelNegrete@users.noreply.github.com>
2018-10-17 12:44:02 -0500

    Animacion Modelos
    se agregaron efecos de sangre, animacion de cerebro y corazon y un script de rotacion

---
+++ MedicineRA/Assets/Scripts/modelRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modelRotation : MonoBehaviour {
+
+	public float rotationSpeed;
+	public bool rotate;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if(rotate)
+		{
+			gameObject.transform.Rotate(new Vector3(0, rotationSpeed, 0));
+		}
+	}
+}
